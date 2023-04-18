f78e67b74934352c2f25a48b389b810ca90da069
Ira Hayes <ira@ideapros.com>
2023-04-18 12:50:34 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "test-40015",
-  url: "https://test-40015.botics.co",
-  api: "https://test-40015.botics.co/api/v1"
+  "name": "test-40015",
+  "url": "https://test-40015.botics.co",
+  "api": "https://test-40015.botics.co/api/v1",
+  "initialRoute": "Untitled1"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
